26299a66d1ce9c754dd6a7ec28b22cc5e8e9da41
Hope Zouzas <hmzouzas@gmail.com>
2018-07-11 18:43:17 +0000

    automatically backed up by learn

--- room.rb
+++ room.rb
@@ -15,3 +15,11 @@ numbers.each do |x|
 end 
 
   puts new_numbers
+  
+  tax_included = []
+  cart_item_price = [12.50, 19.99, 3.49,7.99]
+  cart_item_price.each do |price|
+    price_with_tax = price * 1.17
+    tax_included << price_with_tax
+  end 
+  
