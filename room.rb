3434f4e8e254ca99d5ff4cc1f9153d49a18493f0
Hope Zouzas <hmzouzas@gmail.com>
2018-07-11 18:08:23 +0000

    automatically backed up by learn

--- room.rb
+++ room.rb
@@ -2,7 +2,15 @@ room = ["beyonce", "cardi", "adele"]
 room.insert(1, "Taylor S.")
 puts room
 
-numbers = [1..100]
+numbers = [1,2,3,4]
 numbers.each do |x|
   puts x 
 end
+
+new_numbers = []
+numbers = [1,2,3,4]
+numbers.each do |x|
+  new_number = x + 1 
+  new_numbers << new_numbers
+end 
+puts new_numbers
